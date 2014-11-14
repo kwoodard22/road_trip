16a458432271fccc68e10efcbc25df4f9ffa7b6e
paulturaew <paul.turaew@gmail.com>
2014-11-14 15:21:23 -0500

    1st set of rspec testing for welcome page complete

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -3,18 +3,43 @@
 require 'rails_helper'
 require 'spec_helper'
 
-feature 'welcome/signup page' do
-  scenario 'welcomes the user & requires signin' do
+
+feature 'welcome page registration' do
+  scenario 'welcomes the user & requires sign in, registration or play' do
 
     visit '/'
-    expect(page.find('h1')).to have_content(/Road Trip/)
-    expect(page).to have_content("Register")
-    expect(page).to have_content("Sign In")
-    expect(page).to have_content("Play")
+    expect(page.find('h1')).to have_content("Road Trip")
+    expect(page).to have_link("Register")
+    expect(page).to have_link("Log in")
+    expect(page).to have_link("Play")
+  end
 
-    click_on 'Register'
-
+  scenario "user clicks on register and goes to registration page" do
+    visit '/'
+    click_link 'Register'
     expect(current_path).to eq(new_user_registration_path)
 
   end
+
+  scenario "user clicks on Log in and goes to log in page" do
+    visit '/'
+    click_link 'Log in'
+    expect(current_path).to eq(new_user_session_path)
+
+  end
+
+  scenario "user clicks on register and goes to play page" do
+    visit '/'
+    click_link 'Play'
+    expect(current_path).to eq(games_index_path)
+
+  end
+
 end
+
+# scenario "user can visit the root page and see links to sign in or register." do
+#     visit root_path
+#     expect(page).to have_link('Register')
+#     click_link 'Register'
+#     expect(current_path).to eq(new_user_path)
+#     expect(page).to have_content("Register Your Metacog Account")
