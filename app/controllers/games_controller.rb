f44634eadbbf147e94547fc4e9fe254858068b64
kwoodard22 <kwoodard22@gmail.com>
2014-11-18 09:19:47 -0500

    game uses JSON now, but Q & A not working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,15 +2,25 @@ class GamesController < ApplicationController
 
   def index
     @all_us_states_info = Game.all
-    @route = ["California", "Arizona", "Connecticut"]
-    @state = Game.where(state: @route.first).first.state
-    @capital = Game.where(state: @route.first).first.capital
-    # Need to figure out how to put this in the model!!
+    state = Game.north_route(0)
+    @capital = Game.where(state:state).first.capital
     capitals_array = []
     @all_us_states_info.each {|s| capitals_array << s.capital }
     capitals_array.delete(@capital)
     @capitals_array = capitals_array
+    session[:stop] = 0
   end
 
+  def next_state
+    state = Game.north_route(params[:stop_num].to_i)
+    capital = Game.where(state: state).first.capital
+    respond_to do |format|
+         format.html
+         format.json { render json: {
+            state: state,
+            capital: capital,
+            }}
+       end
+  end
 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ActiveRecord::Base
+
+  def self.north_route(index)
+    ["California", "Arizona", "New Mexico", "Texas", "Oklahoma", "Arkansas", "Tennessee", "North Carolina"][index]
+  end
 end
